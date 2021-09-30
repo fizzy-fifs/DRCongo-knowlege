7404a1fd1155b16f6a4c0a4a41b16a7d2897c875
W4n1 <wanikalimira@hotmail.co.uk>
2021-09-30 01:36:25 +0000

    JS cleaned up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,126 +1,80 @@
-//event listeners
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons= document.getElementsByTagName("button");
-
-    for(let button of buttons){
-        button.addEventListener("click", function () {
-          if(this.getAttribute("data-type")=== "submit"){
-              alert("You have clicked Next Question!");
-          } else {
-              let optionType = this.getAttribute("data-type");
-              alert(`You clicked ${optionType}`);
-              
-          }
-        })
+const quizQuestions = [
+    {
+      id: 1,
+      question: 'Dr.Congo is situated where in Africa?',
+      options: [
+        'Middle Eastern',
+        'Central Africa',
+        'West Africa',
+        'South Africa',
+      ],
+      correctAnswer: 1,
+    },
+    {
+      id: 2,
+      question: 'The Democratic Republic of Congo was formerly known as?',
+      options: ['Kingdom of Congo', 'Kinshasa', 'Swahili', 'Republic of Zaire'],
+      correctAnswer: 3,
+    },
+    {
+      id: 3,
+      question:
+        'Who took power and changed the name of the country in the 1990s?',
+      options: [
+        'Laurent-Désiré Kabila',
+        'Mobutu Sese Seko',
+        'Joseph Kabila',
+        'Joseph Kasa-Vubu',
+      ],
+      correctAnswer: 0,
+    },
+    {
+      id: 4,
+      question:
+        'In what year and month did Congo their gain independence from Belgium?',
+      options: ['June, 1690s', 'July, 1960s', 'June, 1960s', 'June, 1930s'],
+      correctAnswer: 2,
+    },
+    {
+      id: 5,
+      question: 'What river runs through DR Congo?',
+      options: ['Amazon River', 'Kinshasa River', 'Congo River', 'Wakanda River'],
+      correctAnswer: 2,
+    },
+    {
+      id: 6,
+      question:
+        'In the rankings of the largest African countries, DRC is ranked at what position?',
+      options: ['2nd', '14th', '4th', '3rd'],
+      correctAnswer: 0,
+    },
+  ];
+  
+  let currentIndex = 0;
+  let score = 0;
+  
+  function startQuiz() {
+    nextQuestion();
+    document.getElementById('start-button').disabled = true;
+  }
+  
+  function nextQuestion() {
+    const currentQuestion = quizQuestions[currentIndex];
+  
+    document.getElementById('question-text').innerHTML = currentQuestion.question;
+    document.getElementById('option1').innerHTML = currentQuestion.options[0];
+    document.getElementById('option2').innerHTML = currentQuestion.options[1];
+    document.getElementById('option3').innerHTML = currentQuestion.options[2];
+    document.getElementById('option4').innerHTML = currentQuestion.options[3];
+  
+    currentIndex++;
+  }
+  
+  function clickOption(selectedOption) {
+    const currentQuestion = quizQuestions[currentIndex];
+    if (currentQuestion.correctAnswer === selectedOption) {
+      score++;
+      document.getElementById('score-value').innerHTML = score;
     }
-})
-
-//this function will run the quiz
-function runQuiz(){
-    let centeredH1= document.getElementsByTagName('h1')[0];
-    centeredH1.style.textAlign="center";
-    let centeredP= document.getElementsByTagName('p')[0];
-    centeredP.style.textAlign="center";
-    
-    let questionContainer= document.getElementsByClassName('question-container');
-    let resultContainer= document.getElementById('results');
-    let submitButton= document.getElementsById('submit');
-//displaying the Quiz Questions, Options and correct answers const quizQuestions=[
-
-   let quizQuestions =[
-       {
-        question:"Dr.Congo is situated where in Africa?",
-        answers: {
-            a:"Middle Eastern",
-            b:"Central Africa",
-            c:"West Africa",
-            d:"South Africa",
-                },
-        correctAnswers: "b"
-       },
-       {
-        question:"DR Congo was formerly known as?",
-        answers: {
-            a:"Kingdom of Congo",
-            b:"Kinshasa",
-            c:"Swahili",
-            d:"Republic of Zaire",
-                },
-        correctAnswers: "d"
-/*Republic of Zaire or République du Zaïre in French. 
-if I have time to include the reasoning or why the country had changed names*/
-    },
-    {
-        question:"Who took power and changed the name of the country in the 1990s?",
-        answers: {
-            a:"Laurent-Désiré Kabila",
-            b:"Mobutu Sese Seko",
-            c:"Joseph Kabila",
-            d:"Joseph Kasa-Vubu",
-                },
-        correctAnswers: "a"
-        /*could add the following for extra intel- [All names mentioned in the options ruled the country at some point,
-the longest time in office being president Mobutu with over 31 years spent in power and the shortest being Kabila. 
-Now under new Presidency with Félix Tshisekedi since 2019].*/
-    },
-    {
-        question:"In what year and month did Congo their gain independence from Belgium?",
-        answers: {
-            a:"June, 1690s",
-            b:"July 1960s",
-            c:"June 1960s",
-            d:"June 1930s",
-                },
-        correctAnswers: "c" //30, June 1960s
-
-     },
-     {
-        question:"What river runs through DR Congo?",
-        answers: {
-            a:"Amazon River",
-            b:"Kinshasa River",
-            c:"Congo River",
-            d:"Wakanda River",
-                },
-        correctAnswers: "c"
-                    // Congo River. Formerly known as the Zaire River, second longest River in Africa and the World's deepest recorded river.
-    },
-    {
-        question:"In the rankings of the largest African countries, DRC is ranked at what position?",
-        answers: {
-            a:"2nd",
-            b:"14th",
-            c:"4th",
-            d:"3rd",
-                },
-        correctAnswers: "a"
-    }, // make ALL the answers hidden from user.
-   ]
-
-}
-
-
-//this will be used to check the users answer
-function checkAnswer(){
-
-}
-
-//for correct answer count to be increased
-function correctAnswer(){
-    
-}
-
-
-//for wrong answer count to be decreased/
-function wrongAnswer(){
-    
-}
-
-function displayQuestion() {
-    
-}
-
-function displayOptions() {
-    
-}
-
+      nextQuestion();
+  }
